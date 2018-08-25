0d843e99f2db5449efe59a9b2791f4477c3fa20e
Keshigom <taiseiishii141@gmail.com>
2018-08-26 00:25:23 +0900

    読み込まれない不具合の修正

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,16 +1,27 @@
+
+$(window).on('load', () => {
+    //Material Desgin Liteの影響でiframe要素は対応が必要
+    //モバイルでmdl-componentupgradedが呼ばれなかった？
+    //とりあえずこっちでも実行できる
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  });
+
 jQuery(function () {
 
     //Material Desgin Liteの影響でiframe要素は対応が必要
-    $('.mdl-layout').on('mdl-componentupgraded', function (e) {
-        if ($(e.target).hasClass('mdl-layout')) {
-            // IFrame Player API の読み込み
-            console.log("mdl-comupg");
-            var tag = document.createElement('script');
-            tag.src = "https://www.youtube.com/iframe_api";
-            var firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        }
-    });
+    // $('.mdl-layout').on('mdl-componentupgraded', function (e) {
+    //     if ($(e.target).hasClass('mdl-layout')) {
+    //         // IFrame Player API の読み込み
+    //         console.log("mdl-comupg");
+    //         var tag = document.createElement('script');
+    //         tag.src = "https://www.youtube.com/iframe_api";
+    //         var firstScriptTag = document.getElementsByTagName('script')[0];
+    //         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    //     }
+    // });
 
     //アコーディオンメニュー
     $('.accordion').click(function () {
